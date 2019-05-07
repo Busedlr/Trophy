77f565be38c56d23ebebb163a6556616f927e5ca
Busedlr <b.dilara@live.com>
2019-05-07 12:22:46 +0200

    second

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, HostListener } from "@angular/core";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
+})
+export class HeaderComponent implements OnInit {
+  constructor() {
+    /* @HostListener()  */
+  }
+  ngOnInit() {}
+
+  @HostListener("window:scroll", ["$event"])
+  scrollHandler(event) {
+    let navbar = document.getElementById("navbar");
+    let sticky = navbar.offsetTop;
+
+    console.log("window.pageYOffset", window.pageYOffset);
+    console.log("sticky", sticky);
+
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } 
+
+    /* value must correspond to ".header-image => height" in style.css */
+    if (window.pageYOffset < 320) {
+      navbar.classList.remove("sticky");
+    }
+  }
+}
