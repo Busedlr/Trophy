2931f4cdb2c69a2328c3beea760fedd26a1c5200
Busedlr <b.dilara@live.com>
2019-05-27 21:04:07 +0200

    scroll top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,16 @@ export class AppComponent {
   constructor() {
     firebase.initializeApp(firebaseConfig);
   }
+
+
+  scrollTop() {
+    let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20);
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);
+}
 }
